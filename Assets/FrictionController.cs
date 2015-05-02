50c1ca8dafb9b285a9af7e4503144dd7c4f4d0db
TuanHoang277 <sotuanhoang@gmail.com>
2015-05-02 17:09:46 +0700

    Friction and Physic Material ice, glue, bouncy

---
+++ Assets/FrictionController.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FrictionController : MonoBehaviour {
+
+    public TextMesh txtTimer;
+    private float timeCur;
+
+    // Use this for initialization
+    void Start()
+    {
+        timeCur = 0.0f;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        timeCur += Time.deltaTime;
+        txtTimer.text = System.String.Format("Time = {0}", timeCur);
+    }
+}
---
+++ Assets/Timer.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Timer : MonoBehaviour {
+
+    public TextMesh txtTimer;
+    private float timeCur;
+	// Use this for initialization
+	void Start () {
+        timeCur = 0.0f;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        timeCur += Time.deltaTime;
+	}
+}
